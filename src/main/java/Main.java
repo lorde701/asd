b2269eeb71a7ef9533b4c0b1adb56f08d30a504d
Ivanna Stetsyuk <istetsyuk@aplana.com>
2019-05-20 14:36:11 +0300

    asdf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет");
-        System.out.println(System.getProperties().getProperty("params"));
+        System.out.println("Hi! sfhlksflkjslas;dm;asmd;amlsclmz/c/l,sl,c'asc");
+//        System.out.println(System.getProperties().getProperty("params"));
     }
 }
